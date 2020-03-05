e68d8a2e11fc1770d05ba4062974046352c9bddd
gabrielmg1997 <adm.gabriel2009@gmail.com>
2020-03-05 01:59:59 -0300

    reparos na Versão 0.1

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 #-------------------------------------------------
 #Atualizando os Repo e procurando atualizações
+sudo apt --fix-broken install -y
+sudo apt remove firefox
+sudo apt remove spectacle
+sudo apt remove kcalc
 sudo apt update -y && sudo apt dist-upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y
 sudo add-apt-repository ppa:papirus/papirus -y 
 sudo apt-get update -y 
@@ -16,16 +20,25 @@ git clone git://anongit.kde.org/latte-dock
 #instalação
 dpkg -i virtual.deb
 dpkg -i chrome.deb
+sudo apt --fix-broken install -y
 sudo apt install telegram-desktop -y
-sudo apt install flameshot -y 
+sudo apt --fix-broken install -y
+sudo apt install flameshot -y
+sudo apt --fix-broken install -y
 sudo apt install neofetch -y
+sudo apt --fix-broken install -y
 sudo apt install kvantum -y 
+sudo apt --fix-broken install -y
 sudo apt install tilda -y
+sudo apt --fix-broken install -y
 sudo apt install mousepad -y
+sudo apt --fix-broken install
 sudo apt install gnome-mvp -y
+sudo apt --fix-broken install
 sudo apt install audacious -y
+sudo apt --fix-broken install
 sudo apt install cmake extra-cmake-modules qtdeclarative5-dev libqt5x11extras5-dev libkf5iconthemes-dev libkf5plasma-dev libkf5windowsystem-dev libkf5declarative-dev libkf5xmlgui-dev libkf5activities-dev build-essential libxcb-util-dev libkf5wayland-dev git gettext libkf5archive-dev libkf5notifications-dev libxcb-util0-dev libsm-dev libkf5crash-dev libkf5newstuff-dev -y
-
+sudo apt --fix-broken install
 #-------------------------------------------------
 #opera
 dpkg -i opera.deb
@@ -34,21 +47,27 @@ cp libffmpeg.so /usr/lib/x86_64-linux-gnu/opera
 #--------------------------------------------------
 #theme
 cd McMojave-kde
-./install
+sudo ./install.sh -y
 cd ..
 #---------------------------------------------------
 #latte-dock
 cd latte-dock
-./install 
+sudo ./install.sh  
 cd .. 
+﻿plasmapkg2 --install org.kde.plasma.dittomenu.plasmoid
+plasmapkg2 --install applet-latte-separator-0.1.1.plasmoid
+plasmapkg2 --install presentwindows-v8.plasmoid
 #---------------------------------------------------
 #tilda/neof/latte/konsole/korla/speed/
 cp -r tilda /home/gabriel/.config/
 cp -r neofetch /home/gabriel/.config/
 cp -r latte /home/gabriel/.config/
-mv -r speedtest /home/gabriel/
+cp -r kdeglobals /home/gabriel/.kde/share/config/
+cp -r speedtest /home/gabriel/
 cp -r korla /home/gabriel/.local/share/icons/
 cp -r konsole /home/gabriel/.local/share/
+#cp -r com.github.vinceliuice.McMojave /home/gabriel/.local/share/plasma/look-and-feel/
+
 
 
 
